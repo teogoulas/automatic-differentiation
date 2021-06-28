66cadffb8866da7e013b832efe345b3d32b3ac17
Thodoris Goulas <t.goulas@paymentcomponents.com>
2021-06-29 00:35:12 +0300

    Refactoring.

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
-# importing the libraries
-import pandas as pd
-import numpy as np
-## from tqdm import tqdm
-
-# for reading and displaying images
-## from skimage.io import imread
-## from skimage.transform import resize
-## import matplotlib.pyplot as plt
-
-
-# for creating validation set
-from sklearn.model_selection import train_test_split
-
-# for evaluating the model
-from sklearn.metrics import accuracy_score
-
 # PyTorch libraries and modules
 import torch
 from torch.autograd import Variable
-from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, \
-    Dropout, BatchNorm1d, BCELoss, Sigmoid, Flatten
-from torch.optim import Adam, SGD
-from torch.utils.data import Dataset, DataLoader
+from torch.nn import CrossEntropyLoss
+from torch.optim import Adam
+from torch.utils.data import Dataset
 
 # torchvision for pre-trained models
-from torchvision import models, datasets, transforms
-from models.NnModel import NnModel
+from torchvision import datasets, transforms
 from models.CnnModel import CnnModel
-from utils.batchUtils import generate_batches
-from utils.clfUtils import binary_acc, view_classify
+from utils.clfUtils import view_classify
 
 
 def main():
@@ -46,61 +26,29 @@ def main():
     print(device)
 
     base_model = CnnModel()
-
-    base_model.train()
-
     criterion = CrossEntropyLoss()
     optimizer = Adam(base_model.parameters(), lr=0.001)
+
+    base_model.train()
     for e in range(1, 11):
         epoch_loss = 0
-        epoch_loss_curve = 0
-        epoch_loss_line = 0
-
         epoch_acc = 0
-        epoch_acc_curve = 0
-        epoch_acc_line = 0
         for X_batch, y_batch in trainloader:
-            # y_batch_circle, y_batch_curve, y_batch_line = generate_batches(y_batch)
             X_batch = Variable(X_batch).float()
             y_batch = Variable(y_batch)
 
             optimizer.zero_grad()
-            # curve_optimizer.zero_grad()
-            # line_optimizer.zero_grad()
-
             y_pred, _, _, _ = base_model(X_batch)
-            # y_pred_curve = curve_model(X_batch)
-            # y_pred_line = line_model(X_batch)
 
             loss = criterion(y_pred, y_batch)
-            # loss_curve = criterion(y_pred_curve, y_batch_curve.unsqueeze(1))
-            # loss_line = criterion(y_pred_line, y_batch_line.unsqueeze(1))
-
-            # acc_curve = binary_acc(y_pred_curve, y_batch_curve.unsqueeze(1))
-            # acc_line = binary_acc(y_pred_line, y_batch_line.unsqueeze(1))
-
             loss.backward()
-            # loss_curve.backward()
-            # loss_line.backward()
-
             optimizer.step()
-            # curve_optimizer.step()
-            # line_optimizer.step()
 
             epoch_loss += loss.item()
-            # epoch_loss_curve += loss_curve.item()
-            # epoch_loss_line += loss_line.item()
-
             epoch_acc += (torch.max(y_pred.data, 1)[1] == y_batch).sum()
-            # poch_acc_curve += acc_curve.item()
-            # poch_acc_line += acc_line.item()
 
         print(
             f'Circle: Epoch {e + 0:03}: | Loss: {epoch_loss / len(trainloader):.5f} | Acc: {epoch_acc / len(trainloader):.3f}')
-        # print(
-        #     f'Curve: Epoch {e + 0:03}: | Loss: {epoch_loss_curve / len(trainloader):.5f} | Acc: {epoch_acc_curve / len(trainloader):.3f}')
-        # print(
-        #     f'Line: Epoch {e + 0:03}: | Loss: {epoch_loss_line / len(trainloader):.5f} | Acc: {epoch_acc_line / len(trainloader):.3f}')
 
     images, labels = next(iter(valloader))
 
@@ -121,9 +69,9 @@ def main():
     print("Is Curve =", probab.index(max(probab_curve)))
     print("Is Line =", probab.index(max(probab_line)))
     view_classify(img.view(1, 28, 28), ps, 10)
-    view_classify(img.view(1, 28, 28), ps_circle, 10)
-    view_classify(img.view(1, 28, 28), ps_curve, 10)
-    view_classify(img.view(1, 28, 28), ps_line, 10)
+    view_classify(img.view(1, 28, 28), ps_circle, 2)
+    view_classify(img.view(1, 28, 28), ps_curve, 2)
+    view_classify(img.view(1, 28, 28), ps_line, 2)
 
 
 if __name__ == '__main__':
--- models/CnnModel.py
+++ models/CnnModel.py
@@ -1,8 +1,8 @@
-from torch.nn import Linear, ReLU, Sequential, Conv2d, MaxPool2d, Module, Dropout, Sigmoid, Flatten, Softmax
+from torch.nn import Linear, ReLU, Sequential, Conv2d, MaxPool2d, Module, Sigmoid, Flatten, Softmax
 
 from utils.batchUtils import split_batches
 
-model = Sequential(
+base_model = Sequential(
     Conv2d(
         in_channels=1,
         out_channels=16,
@@ -26,14 +26,38 @@ model = Sequential(
     Softmax()
 )
 
+binary_model = Sequential(
+    Conv2d(
+        in_channels=1,
+        out_channels=16,
+        kernel_size=(5, 5),
+        stride=1,
+        padding=2,
+    ),
+    ReLU(),
+    MaxPool2d(kernel_size=2),
+    Conv2d(
+        in_channels=16,
+        out_channels=32,
+        kernel_size=(5, 5),
+        stride=1,
+        padding=2
+    ),
+    ReLU(),
+    MaxPool2d(2),
+    Flatten(),
+    Linear(32 * 7 * 7, 1),
+    Sigmoid()
+)
+
 
 class CnnModel(Module):
     def __init__(self):
         super(CnnModel, self).__init__()
-        self.base_model = model
-        self.circle_model = model
-        self.curve_model = model
-        self.line_model = model
+        self.base_model = base_model
+        self.circle_model = binary_model
+        self.curve_model = binary_model
+        self.line_model = binary_model
 
     def forward(self, x):
         output = self.base_model(x)
